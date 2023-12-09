ad25e093100b4f6c944de8cf2f69a612a3c41011
Nico Ries <nicories@posteo.de>
2023-12-09 10:57:38 +0100

    AOC23 day 9 part 2

--- aoc23/src/bin/day_09.rs
+++ aoc23/src/bin/day_09.rs
@@ -30,7 +30,7 @@ fn extrapolate_data(data: &Vec<i32>) -> Vec<Vec<i32>> {
     }
     v
 }
-fn finish_data(data: &mut Vec<Vec<i32>>) {
+fn finish_data_forward(data: &mut Vec<Vec<i32>>) {
     let len = data.len();
     data[len - 1].push(0);
     for i in (0..len - 1).rev() {
@@ -39,9 +39,29 @@ fn finish_data(data: &mut Vec<Vec<i32>>) {
         data[i].push(left_val + low_val);
     }
 }
+fn finish_data_backwards(data: &mut Vec<Vec<i32>>) {
+    let len = data.len();
+    data[len - 1].push(0);
+    for i in (0..len - 1).rev() {
+        let left_val = *data[i].first().unwrap();
+        let low_val = *data[i + 1].first().unwrap();
+        data[i].insert(0, left_val - low_val);
+    }
+}
 
-fn part_two(input: &str) -> u64 {
-    0
+fn part_two(input: &str) -> i32 {
+    let sensor_data = parse_sensor_data(input);
+    let mut extrapolated_data = vec![];
+    for data in sensor_data {
+        extrapolated_data.push(extrapolate_data(&data));
+    }
+    for data in &mut extrapolated_data {
+        finish_data_backwards(data);
+    }
+    extrapolated_data
+        .iter()
+        .map(|d| d[0].first().unwrap())
+        .sum()
 }
 
 fn part_one(input: &str) -> i32 {
@@ -51,7 +71,7 @@ fn part_one(input: &str) -> i32 {
         extrapolated_data.push(extrapolate_data(&data));
     }
     for data in &mut extrapolated_data {
-        finish_data(data);
+        finish_data_forward(data);
     }
     extrapolated_data.iter().map(|d| d[0].last().unwrap()).sum()
 }
@@ -78,11 +98,20 @@ mod tests {
         assert_eq!(extrapolated_data[0][1], vec![3, 3, 3, 3, 3]);
         assert_eq!(extrapolated_data[0][2], vec![0, 0, 0, 0]);
         for data in &mut extrapolated_data {
-            finish_data(data);
+            finish_data_forward(data);
         }
         dbg!(&extrapolated_data[0]);
         assert_eq!(extrapolated_data[0][0], vec![0, 3, 6, 9, 12, 15, 18]);
 
         assert_eq!(part_one(INPUT_TEST), 114);
+        assert_eq!(part_one(INPUT), 1696140818);
+
+        for data in &mut extrapolated_data {
+            finish_data_backwards(data);
+        }
+        assert_eq!(extrapolated_data[2][0], vec![5, 10, 13, 16, 21, 30, 45, 68]);
+
+        assert_eq!(part_two(INPUT_TEST), 2);
+        assert_eq!(part_two(INPUT), 1152);
     }
 }
