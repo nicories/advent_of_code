60b2d430d23f79435bd1f4343ac9be586326f26f
Nico Ries <nicories@posteo.de>
2023-12-13 11:13:11 +0100

    AOC23 day 13 part 2

--- aoc23/src/bin/day_13.rs
+++ aoc23/src/bin/day_13.rs
@@ -45,47 +45,89 @@ fn reflections_line(line: &str) -> Vec<(usize, usize)> {
     }
     v
 }
+fn common_reflections(block: &str) -> Vec<(usize, usize)> {
+    let mut common_reflections = vec![];
+    let all_reflections: Vec<Vec<(usize, usize)>> =
+        block.lines().map(|line| reflections_line(line)).collect();
+    if all_reflections.len() != block.lines().count() {
+        return common_reflections;
+    }
+    let first_reflections = all_reflections.first().unwrap();
+    for reflection in first_reflections {
+        if all_reflections.iter().all(|x| x.contains(reflection)) {
+            common_reflections.push((reflection.0, reflection.1));
+        }
+    }
 
-fn block_value(input: &str) -> usize {
-    let mut sum = 0;
-    let horizontal_reflections: Vec<Vec<(usize, usize)>> =
-        input.lines().map(|line| reflections_line(line)).collect();
-    for v in &horizontal_reflections {
-        if let Some(x) = v
-            .iter()
-            .find(|x| horizontal_reflections.iter().all(|z| z.contains(&x)))
-        {
-            sum += x.0 + 1;
-            break;
-        }
-    }
-    let vertical_lines = transpose_lines(input);
-    let vertical_reflections: Vec<Vec<(usize, usize)>> = vertical_lines
-        .lines()
-        .map(|line| reflections_line(line))
-        .collect();
-    for v in &vertical_reflections {
-        if let Some(x) = v
-            .iter()
-            .find(|x| vertical_reflections.iter().all(|z| z.contains(&x)))
-        {
-            sum += (x.0 + 1) * 100;
-            break;
-        }
-    }
-    sum
+    common_reflections
+}
+fn common_reflections_horizontal(block: &str) -> Vec<(usize, usize)> {
+    common_reflections(block)
+}
+fn common_reflections_vertical(block: &str) -> Vec<(usize, usize)> {
+    let vertical_lines_original = transpose_lines(block);
+    common_reflections(&vertical_lines_original)
 }
 
-fn parse(input: &str) -> usize {
-    input.split("\n\n").map(|block| block_value(block)).sum()
+fn block_permutations(input: &str) -> Vec<String> {
+    let mut v = vec![];
+    for (i, c) in input.chars().enumerate() {
+        let mut new_string = String::from(input);
+        if c == '.' {
+            new_string.replace_range(i..=i, "#");
+            v.push(new_string);
+        } else if c == '#' {
+            new_string.replace_range(i..=i, ".");
+            v.push(new_string);
+        }
+    }
+    v
+}
+
+fn block_value_smudge(input: &str) -> usize {
+    let horizontal_original = common_reflections_horizontal(input);
+    let vertical_original = common_reflections_vertical(input);
+    let permutations = block_permutations(input);
+    for perm in &permutations {
+        let horizontal = common_reflections_horizontal(&perm);
+        let vertical = common_reflections_vertical(&perm);
+        if let Some(h) = horizontal
+            .iter()
+            .filter(|x| !horizontal_original.contains(x))
+            .next()
+        {
+            return h.0 + 1;
+        } else if let Some(v) = vertical
+            .iter()
+            .filter(|x| !vertical_original.contains(x))
+            .next()
+        {
+            return (v.0 + 1) * 100;
+        }
+    }
+    panic!("at the disco");
+}
+
+fn block_value(input: &str) -> usize {
+    let horizontal_reflections = common_reflections(input);
+    let vertical_lines = transpose_lines(input);
+    let vertical_reflections = common_reflections(&vertical_lines);
+    if let Some(horizontal) = horizontal_reflections.first() {
+        return horizontal.0 + 1;
+    } else {
+        return (vertical_reflections.first().unwrap().0 + 1) * 100;
+    }
 }
 
 fn part_two(input: &str) -> usize {
-    parse(input)
+    input
+        .split("\n\n")
+        .map(|block| block_value_smudge(block))
+        .sum()
 }
 
 fn part_one(input: &str) -> usize {
-    parse(input)
+    input.split("\n\n").map(|block| block_value(block)).sum()
 }
 fn main() {
     println!("1: {}", part_one(INPUT));
@@ -101,16 +143,17 @@ mod tests {
 
     #[test]
     fn test() {
-        // assert_eq!(reflections_line("#.##..##."), vec![(4, 4), (6, 2)]);
-        // assert_eq!(reflections_line("..#.##.#."), vec![(0, 1), (4, 4)]);
-        // assert_eq!(reflections_line("..##..."), vec![(0, 1), (2, 3), (5, 1)]);
-        // assert_eq!(block_value(INPUT_TEST.split("\n\n").nth(0).unwrap()), 5);
-        // assert_eq!(block_value(INPUT_TEST.split("\n\n").nth(1).unwrap()), 400);
+        assert_eq!(reflections_line("#.##..##."), vec![(4, 4), (6, 2)]);
+        assert_eq!(reflections_line("..#.##.#."), vec![(0, 1), (4, 4)]);
+        assert_eq!(reflections_line("..##..."), vec![(0, 1), (2, 3), (5, 1)]);
+        assert_eq!(reflections_line("##....#..#."), vec![(0, 1), (7, 3)]);
+        assert_eq!(block_value(INPUT_TEST.split("\n\n").nth(0).unwrap()), 5);
+        assert_eq!(block_value(INPUT_TEST.split("\n\n").nth(1).unwrap()), 400);
 
         assert_eq!(part_one(INPUT_TEST), 405);
-        // assert_eq!(part_one(INPUT), 9647174);
-        // assert_eq!(galaxy_paths(INPUT_TEST, 10), 1030);
-        // assert_eq!(galaxy_paths(INPUT_TEST, 100), 8410);
-        // assert_eq!(part_two(INPUT), 377318892554);
+        assert_eq!(part_one(INPUT), 27505);
+
+        assert_eq!(part_two(INPUT_TEST), 400);
+        assert_eq!(part_two(INPUT), 22906);
     }
 }
