83fbb895235dc980ac5c449517d0641c47396e07
Nico Ries <nicories@posteo.de>
2023-12-20 05:09:58 +0100

    AOC23 day 19 part 2

--- aoc23/src/bin/day_19.rs
+++ aoc23/src/bin/day_19.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Range};
 
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/19_full.txt"));
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 enum WorkFlowResult {
     Rejected,
     Accepted,
@@ -124,47 +124,150 @@ fn parse(input: &str) -> (Vec<Part>, HashMap<String, Vec<Rule>>) {
         .map(|line| &line[1..line.len() - 1])
         // ugly
         .map(|line| {
-            let mut split = line.split(',').map(|x| x.trim());
-            let x = split
-                .next()
-                .unwrap()
-                .split_once('=')
-                .unwrap()
-                .1
-                .parse()
-                .unwrap();
-            let m = split
-                .next()
-                .unwrap()
-                .split_once('=')
-                .unwrap()
-                .1
-                .parse()
-                .unwrap();
-            let a = split
-                .next()
-                .unwrap()
-                .split_once('=')
-                .unwrap()
-                .1
-                .parse()
-                .unwrap();
-            let s = split
-                .next()
-                .unwrap()
-                .split_once('=')
-                .unwrap()
-                .1
-                .parse()
-                .unwrap();
+            let v: Vec<usize> = line
+                .split(',')
+                .map(|x| x.trim())
+                .map(|entry| entry.split_once('=').unwrap().1.parse().unwrap())
+                .collect();
+            let x = v[0];
+            let m= v[1];
+            let a = v[2];
+            let s = v[3];
             Part { x, m, a, s }
         })
         .collect();
     (parts, workflows)
 }
 
+#[derive(Debug)]
+struct AcceptedRange {
+    x_low: usize,
+    x_high: usize,
+    m_low: usize,
+    m_high: usize,
+    a_low: usize,
+    a_high: usize,
+    s_low: usize,
+    s_high: usize,
+}
+
+impl Default for AcceptedRange {
+    fn default() -> Self {
+        Self {
+            x_low: 1,
+            x_high: 4000,
+            m_low: 1,
+            m_high: 4000,
+            a_low: 1,
+            a_high: 4000,
+            s_low: 1,
+            s_high: 4000,
+        }
+    }
+}
+// to dodge the condition
+fn fix_range_negative(range: &mut AcceptedRange, condition: &Condition) {
+    match (condition.operand_a.as_str(), &condition.operator) {
+        ("x", ConditionOp::GreaterThan) => range.x_high = range.x_high.min(condition.operand_b),
+        ("m", ConditionOp::GreaterThan) => range.m_high = range.m_high.min(condition.operand_b),
+        ("a", ConditionOp::GreaterThan) => range.a_high = range.a_high.min(condition.operand_b),
+        ("s", ConditionOp::GreaterThan) => range.s_high = range.s_high.min(condition.operand_b),
+
+        ("x", ConditionOp::LessThan) => range.x_low = range.x_low.max(condition.operand_b),
+        ("m", ConditionOp::LessThan) => range.m_low = range.m_low.max(condition.operand_b),
+        ("a", ConditionOp::LessThan) => range.a_low = range.a_low.max(condition.operand_b),
+        ("s", ConditionOp::LessThan) => range.s_low = range.s_low.max(condition.operand_b),
+        _ => panic!("at the disco"),
+    }
+}
+// to fulfill the condition
+fn fix_range_positive(range: &mut AcceptedRange, condition: &Condition) {
+    match (condition.operand_a.as_str(), &condition.operator) {
+        ("x", ConditionOp::GreaterThan) => range.x_low = range.x_low.max(condition.operand_b + 1),
+        ("m", ConditionOp::GreaterThan) => range.m_low = range.m_low.max(condition.operand_b + 1),
+        ("a", ConditionOp::GreaterThan) => range.a_low = range.a_low.max(condition.operand_b + 1),
+        ("s", ConditionOp::GreaterThan) => range.s_low = range.s_low.max(condition.operand_b + 1),
+
+        ("x", ConditionOp::LessThan) => range.x_high = range.x_high.min(condition.operand_b - 1),
+        ("m", ConditionOp::LessThan) => range.m_high = range.m_high.min(condition.operand_b - 1),
+        ("a", ConditionOp::LessThan) => range.a_high = range.a_high.min(condition.operand_b - 1),
+        ("s", ConditionOp::LessThan) => range.s_high = range.s_high.min(condition.operand_b - 1),
+        _ => panic!("at the disco"),
+    }
+}
 fn part_two(input: &str) -> usize {
-    0
+    let (_, workflows) = parse(input);
+    let mut ranges: Vec<AcceptedRange> = vec![];
+    for workflow in &workflows {
+        for (i, rule) in workflow.1.iter().enumerate() {
+            if rule.result == WorkFlowResult::Accepted {
+                let mut range = AcceptedRange::default();
+                if let Some(condition) = &rule.condition {
+                    fix_range_positive(&mut range, condition);
+                }
+                // check other conditions
+                for j in 0..i {
+                    if let Some(condition) = &workflow.1[j].condition {
+                        fix_range_negative(&mut range, &condition);
+                    }
+                }
+                let mut previous_name = workflow.0;
+                let mut next_workflow = workflows
+                    .iter()
+                    .filter(|workflow| {
+                        workflow.1.iter().any(|rule| match &rule.result {
+                            WorkFlowResult::Workflow(name) if name == previous_name => true,
+                            _ => false,
+                        })
+                    })
+                    .next()
+                    .unwrap();
+                loop {
+                    for rule in next_workflow.1 {
+                        match &rule.result {
+                            WorkFlowResult::Workflow(name) if name == previous_name => {
+                                if let Some(condition) = &rule.condition {
+                                    fix_range_positive(&mut range, condition);
+                                }
+                                break;
+                            }
+                            WorkFlowResult::Rejected
+                            | WorkFlowResult::Accepted
+                            | WorkFlowResult::Workflow(_) => {
+                                if let Some(condition) = &rule.condition {
+                                    fix_range_negative(&mut range, &condition);
+                                }
+                            }
+                        }
+                    }
+                    if next_workflow.0 == "in" {
+                        break;
+                    }
+                    previous_name = next_workflow.0;
+                    next_workflow = workflows
+                        .iter()
+                        .filter(|workflow| {
+                            workflow.1.iter().any(|rule| match &rule.result {
+                                WorkFlowResult::Workflow(name) if name == previous_name => true,
+                                _ => false,
+                            })
+                        })
+                        .next()
+                        .unwrap();
+                }
+                ranges.push(range);
+            }
+        }
+    }
+    ranges
+        .iter()
+        .map(|range| {
+            (range.x_high - range.x_low + 1)
+                * (range.m_high - range.m_low + 1)
+                * (range.a_high - range.a_low + 1)
+                * (range.s_high - range.s_low + 1)
+        })
+        .sum()
 }
 
 fn part_one(input: &str) -> usize {
@@ -172,10 +275,8 @@ fn part_one(input: &str) -> usize {
     let mut sum = 0;
     for part in parts {
         let mut start = workflows.get("in").unwrap();
-        dbg!(&part);
         loop {
             let result = process_rules(&part, &start);
-            dbg!(&result);
             match result {
                 WorkFlowResult::Rejected => break,
                 WorkFlowResult::Accepted => {
@@ -205,7 +306,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 19114);
         assert_eq!(part_one(INPUT), 406849);
 
-        // assert_eq!(part_two(INPUT_TEST), 952408144115);
-        // assert_eq!(part_two(INPUT), 173152345887206);
+        assert_eq!(part_two(INPUT_TEST), 167409079868000);
+        assert_eq!(part_two(INPUT), 138625360533574);
     }
 }
