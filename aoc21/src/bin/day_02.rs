db6322e7a0ffa6f8c803c2bb4a99257b3250d5c0
Nico Ries <nicories@posteo.de>
2024-02-17 00:44:26 +0100

    AOC21 day 2 part 2

--- aoc21/src/bin/day_02.rs
+++ aoc21/src/bin/day_02.rs
@@ -1,26 +1,39 @@
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/02_full.txt"));
 
 fn part_two(input: &str) -> usize {
-    0
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for line in input.lines() {
+        let (cmd, number) = line.split_once(' ').unwrap();
+        let number: usize = number.parse().unwrap();
+        match cmd {
+            "up" => aim -= number,
+            "down" => aim += number,
+            "forward" => {
+                horizontal += number;
+                depth += number * aim
+            }
+            _ => panic!("at the disco"),
+        }
+    }
+    horizontal * depth
 }
 
 fn part_one(input: &str) -> usize {
-    let up: usize = input
-        .lines()
-        .filter(|s| s.starts_with("up"))
-        .filter_map(|s| s[3..].parse::<usize>().ok())
-        .sum();
-    let down: usize = input
-        .lines()
-        .filter(|s| s.starts_with("down"))
-        .filter_map(|s| s[5..].parse::<usize>().ok())
-        .sum();
-    let forward: usize = input
-        .lines()
-        .filter(|s| s.starts_with("forward"))
-        .filter_map(|s| s[8..].parse::<usize>().ok())
-        .sum();
-    (down - up) * forward
+    let mut horizontal = 0;
+    let mut depth = 0;
+    for line in input.lines() {
+        let (cmd, number) = line.split_once(' ').unwrap();
+        let number: usize = number.parse().unwrap();
+        match cmd {
+            "up" => depth -= number,
+            "down" => depth += number,
+            "forward" => horizontal += number,
+            _ => panic!("at the disco"),
+        }
+    }
+    horizontal * depth
 }
 fn main() {
     println!("1: {}", part_one(INPUT));
@@ -39,7 +52,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 150);
         assert_eq!(part_one(INPUT), 1654760);
 
-        // assert_eq!(part_two(INPUT_TEST), 5);
-        // assert_eq!(part_two(INPUT), 1538);
+        assert_eq!(part_two(INPUT_TEST), 900);
+        assert_eq!(part_two(INPUT), 1956047400);
     }
 }
