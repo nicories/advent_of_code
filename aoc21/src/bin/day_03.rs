b025b5c482aa3d9905a62a1a5ff78326b26c4e13
Nico Ries <nicories@posteo.de>
2024-02-17 01:34:43 +0100

    AOC21 day 3 part 2

--- aoc21/src/bin/day_03.rs
+++ aoc21/src/bin/day_03.rs
@@ -1,7 +1,27 @@
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/03_full.txt"));
 
-fn part_two(input: &str) -> usize {
-    0
+fn bit_criteria(input: &str, when_most_zeroes: char, when_most_ones: char) -> u32 {
+    let mut input_vec: Vec<String> = input.lines().map(|s| s.to_string()).collect();
+    let mut index = 0;
+    while input_vec.len() != 1 {
+        let zeroes = input_vec
+            .iter()
+            .filter(|line| line.chars().nth(index).unwrap() == '0')
+            .count();
+        let keep = if zeroes > input_vec.len() / 2 {
+            when_most_zeroes
+        } else {
+            when_most_ones
+        };
+        input_vec.retain(|line| line.chars().nth(index).unwrap() == keep);
+        index += 1;
+    }
+    u32::from_str_radix(&input_vec[0], 2).unwrap()
+}
+fn part_two(input: &str) -> u32 {
+    let oxygen_rating = bit_criteria(input, '0', '1');
+    let co2_rating = bit_criteria(input, '1', '0');
+    oxygen_rating * co2_rating
 }
 
 fn part_one(input: &str) -> u32 {
@@ -40,7 +60,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 198);
         assert_eq!(part_one(INPUT), 852500);
 
-        // assert_eq!(part_two(INPUT_TEST), 900);
-        // assert_eq!(part_two(INPUT), 1956047400);
+        assert_eq!(part_two(INPUT_TEST), 230);
+        assert_eq!(part_two(INPUT), 1007985);
     }
 }
