d4ff03e1bbcf0169a4858821bfec0bd22a6def6e
Nico Ries <nicories@posteo.de>
2024-02-17 05:27:08 +0100

    AOC21 day 7 part 2

--- aoc21/src/bin/day_07.rs
+++ aoc21/src/bin/day_07.rs
@@ -1,7 +1,23 @@
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/07_full.txt"));
 
-fn part_two(input: &str) -> usize {
-    0
+fn part_two(input: &str) -> u32 {
+    let crabs: Vec<u32> = input
+        .trim()
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let mut min_fuel = u32::MAX;
+    let min_crab = *crabs.iter().min().unwrap();
+    let max_crab = *crabs.iter().max().unwrap();
+    for horizontal in min_crab..=max_crab {
+        min_fuel = min_fuel.min(
+            crabs
+                .iter()
+                .map(|crab| (0..=(crab.max(&horizontal) - crab.min(&horizontal))).sum::<u32>())
+                .sum(),
+        );
+    }
+    min_fuel
 }
 fn part_one(input: &str) -> u32 {
     let crabs: Vec<u32> = input
@@ -39,7 +55,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 37);
         assert_eq!(part_one(INPUT), 342534);
 
-        // assert_eq!(part_two(INPUT_TEST), 26984457539);
-        // assert_eq!(part_two(INPUT), 1632146183902);
+        assert_eq!(part_two(INPUT_TEST), 168);
+        assert_eq!(part_two(INPUT), 94004208);
     }
 }
