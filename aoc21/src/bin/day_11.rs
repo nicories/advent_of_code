2853a89e80eff8a7d7106ceb8c5451b3b42153c9
Nico Ries <nicories@posteo.de>
2024-02-18 22:47:19 +0100

    AOC21 day 11 part 2

--- aoc21/src/bin/day_11.rs
+++ aoc21/src/bin/day_11.rs
@@ -2,8 +2,73 @@ use std::collections::HashSet;
 
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/11_full.txt"));
 
+fn check_simultaneous_flash(grid: &Vec<Vec<u32>>) -> bool {
+    for line in grid {
+        for octopus in line {
+            if *octopus != 0 {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn simulate_step(grid: &mut Vec<Vec<u32>>) -> usize {
+    // increase each by 1
+    for line in &mut *grid {
+        for octopus in line {
+            *octopus += 1;
+        }
+    }
+    let mut flashed_map: HashSet<(usize, usize)> = HashSet::new();
+    loop {
+        let mut flashes = vec![];
+        for y in 0..grid.len() {
+            for x in 0..grid[0].len() {
+                if grid[y][x] > 9 && !flashed_map.contains(&(x, y)) {
+                    flashes.push((x, y));
+                    flashed_map.insert((x, y));
+                }
+            }
+        }
+        // no more flashes -> reset and exit
+        if flashes.is_empty() {
+            for y in 0..grid.len() {
+                for x in 0..grid[0].len() {
+                    if grid[y][x] > 9 {
+                        grid[y][x] = 0;
+                    }
+                }
+            }
+            return flashed_map.len();
+        }
+        // increase neighbors
+        for flash in flashes {
+            for y in flash.1.saturating_sub(1)..=usize::min(flash.1 + 1, grid.len() - 1) {
+                for x in flash.0.saturating_sub(1)..=usize::min(flash.0 + 1, grid[0].len() - 1) {
+                    if x == flash.0 && y == flash.1 {
+                        continue;
+                    }
+                    grid[y][x] += 1;
+                }
+            }
+        }
+    }
+}
+
 fn part_two(input: &str) -> usize {
-    0
+    let mut grid: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect();
+    let mut step = 0;
+    loop {
+        step += 1;
+        simulate_step(&mut grid);
+        if check_simultaneous_flash(&grid) {
+            return step;
+        }
+    }
 }
 
 fn part_one(input: &str) -> usize {
@@ -14,65 +79,7 @@ fn part_one(input: &str) -> usize {
         .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
     for _step in 0..steps {
-        // increase each by 1
-        for line in &mut grid {
-            for octopus in line {
-                *octopus += 1;
-            }
-        }
-        let mut flashed_map: HashSet<(usize, usize)> = HashSet::new();
-        loop {
-            let mut flashes = vec![];
-            for y in 0..grid.len() {
-                for x in 0..grid[0].len() {
-                    if grid[y][x] > 9 && !flashed_map.contains(&(x, y)) {
-                        flashes.push((x, y));
-                        flashed_map.insert((x, y));
-                    }
-                }
-            }
-            // no more flashes -> reset and exit
-            if flashes.is_empty() {
-                number_of_flashes += flashed_map.len();
-                for y in 0..grid.len() {
-                    for x in 0..grid[0].len() {
-                        if grid[y][x] > 9 {
-                            grid[y][x] = 0;
-                        }
-                    }
-                }
-                break;
-            }
-            // increase neighbors
-            for flash in flashes {
-                if flash.1 > 0 {
-                    if flash.0 > 0 {
-                        grid[flash.1 - 1][flash.0 - 1] += 1;
-                    }
-                    grid[flash.1 - 1][flash.0] += 1;
-                    if flash.0 < grid[0].len() - 1 {
-                        grid[flash.1 - 1][flash.0 + 1] += 1;
-                    }
-                }
-
-                if flash.0 > 0 {
-                    grid[flash.1][flash.0 - 1] += 1;
-                }
-                if flash.0 < grid[0].len() - 1 {
-                    grid[flash.1][flash.0 + 1] += 1;
-                }
-
-                if flash.1 < grid.len() - 1 {
-                    if flash.0 > 0 {
-                        grid[flash.1 + 1][flash.0 - 1] += 1;
-                    }
-                    grid[flash.1 + 1][flash.0] += 1;
-                    if flash.0 < grid[0].len() - 1 {
-                        grid[flash.1 + 1][flash.0 + 1] += 1;
-                    }
-                }
-            }
-        }
+        number_of_flashes += simulate_step(&mut grid);
     }
     number_of_flashes
 }
@@ -93,7 +100,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 1656);
         assert_eq!(part_one(INPUT), 1665);
 
-        // assert_eq!(part_two(INPUT_TEST), 288957);
-        // assert_eq!(part_two(INPUT), 2116639949);
+        assert_eq!(part_two(INPUT_TEST), 195);
+        assert_eq!(part_two(INPUT), 235);
     }
 }
