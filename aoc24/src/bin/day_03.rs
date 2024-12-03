a9d01c078d3d3039e206534bcc3b52964633dbd2
Nico Ries <nicories@posteo.de>
2024-12-03 11:28:09 +0100

    AOC24 day 3 part 2

--- aoc24/src/bin/day_03.rs
+++ aoc24/src/bin/day_03.rs
@@ -1,5 +1,36 @@
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/03_full.txt"));
 
+fn filter_enabled(input: &str) -> String {
+    let enable_str = "do()";
+    let disable_str = "don't()";
+
+    let mut in_s = input.to_string();
+    let mut current_enabled = true;
+    let mut out_s = String::new();
+
+    loop {
+        if current_enabled {
+            if let Some(disable_index) = in_s.find(disable_str) {
+                out_s.push_str(&in_s[0..disable_index]);
+                in_s = in_s[disable_index + disable_str.len()..].to_string();
+                current_enabled = false;
+            } else {
+                // end of file
+                out_s.push_str(&in_s[0..]);
+                break;
+            }
+        } else {
+            if let Some(enable_index) = in_s.find(enable_str) {
+                in_s = in_s[enable_index + 1..].to_string();
+                current_enabled = true;
+            } else {
+                // end of file
+                break;
+            }
+        }
+    }
+    out_s
+}
 fn parse(input: &str) -> Vec<(i32, i32)> {
     let mut s = input.to_string();
     let mut pairs = vec![];
@@ -33,8 +64,9 @@ fn parse(input: &str) -> Vec<(i32, i32)> {
     pairs
 }
 
-fn part_two(input: &str) -> usize {
-    0
+fn part_two(input: &str) -> i32 {
+    let filtered_input = filter_enabled(input);
+    part_one(&filtered_input)
 }
 
 fn part_one(input: &str) -> i32 {
@@ -51,13 +83,15 @@ mod tests {
     use super::*;
     const INPUT_TEST: &str =
         include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/03_test.txt"));
+    const INPUT_TEST2: &str =
+        include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/03_test2.txt"));
 
     #[test]
     fn test() {
         assert_eq!(part_one(INPUT_TEST), 161);
         assert_eq!(part_one(INPUT), 175615763);
 
-        // assert_eq!(part_two(INPUT_TEST), 4);
-        // assert_eq!(part_two(INPUT), 426);
+        assert_eq!(part_two(INPUT_TEST2), 48);
+        assert_eq!(part_two(INPUT), 74361272);
     }
 }
