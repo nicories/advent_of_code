80c44bc7f95301336cc3c28f8c33c7e797684120
Nico Ries <nicories@posteo.de>
2024-12-08 22:32:34 +0100

    AOC24 day 4 part 2

--- aoc24/src/bin/day_04.rs
+++ aoc24/src/bin/day_04.rs
@@ -13,11 +13,43 @@ fn vec_get_integer<T>(v: &[T], int: i32) -> Option<&T> {
     }
 }
 
-fn part_two(input: &str) -> i32 {
-    0
+fn find_in_grid(grid: &Grid, offsets: &[[i32; 2]], search_string: &str) -> Vec<(i32, i32)> {
+    grid.iter()
+        .enumerate()
+        .flat_map(|(y, line)| line.iter().enumerate().map(move |(x, _)| (x, y)))
+        .map(|(x, y)| {
+            offsets.iter().filter_map({
+                let grid_ref = grid;
+                move |[x_offset, y_offset]| {
+                    search_string
+                        .chars()
+                        .enumerate()
+                        .all(|(index, c)| {
+                            vec_get_integer(&grid_ref, y as i32 + index as i32 * y_offset)
+                                .is_some_and(|row| {
+                                    vec_get_integer(row, x as i32 + index as i32 * x_offset)
+                                        .is_some_and(|character| *character == c)
+                                })
+                        })
+                        .then_some((x as i32 + x_offset, y as i32 + y_offset)) // return the coords of the second character (i.e. "A" for "MAS")
+                }
+            })
+        })
+        .flatten()
+        .collect()
 }
 
-fn part_one(input: &str) -> i32 {
+fn part_two(input: &str) -> usize {
+    let grid = parse(input);
+    let offsets = [[1, 1], [-1, 1], [-1, -1], [1, -1]];
+    let mut mas: Vec<(i32, i32)> = find_in_grid(&grid, &offsets, "MAS");
+    mas.sort();
+    let before = mas.len();
+    mas.dedup();
+    before - mas.len()
+}
+
+fn part_one(input: &str) -> usize {
     let grid = parse(input);
     let offsets = [
         [1, 0],
@@ -29,30 +61,7 @@ fn part_one(input: &str) -> i32 {
         [0, -1],
         [1, -1],
     ];
-    let chars = "XMAS".chars();
-    grid.iter()
-        .enumerate()
-        .flat_map(|(y, line)| line.iter().enumerate().map(move |(x, _)| (x, y)))
-        .map(|(x, y)| {
-            offsets
-                .iter()
-                .filter_map(|[x_offset, y_offset]| {
-                    chars
-                        .clone()
-                        .enumerate()
-                        .all(|(index, c)| {
-                            vec_get_integer(&grid, y as i32 + index as i32 * y_offset).is_some_and(
-                                |row| {
-                                    vec_get_integer(row, x as i32 + index as i32 * x_offset)
-                                        .is_some_and(|character| *character == c)
-                                },
-                            )
-                        })
-                        .then_some(1)
-                })
-                .sum::<i32>()
-        })
-        .sum()
+    find_in_grid(&grid, &offsets, "XMAS").len()
 }
 fn main() {
     println!("1: {}", part_one(INPUT));
@@ -71,7 +80,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 18);
         assert_eq!(part_one(INPUT), 2521);
 
-        // assert_eq!(part_two(INPUT_TEST), 48);
-        // assert_eq!(part_two(INPUT), 74361272);
+        assert_eq!(part_two(INPUT_TEST), 9);
+        assert_eq!(part_two(INPUT), 1912);
     }
 }
