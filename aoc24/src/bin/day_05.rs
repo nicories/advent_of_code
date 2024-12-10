f4c678e0e953c2afa68eb3509c17f292c0122f5b
Nico Ries <nicories@posteo.de>
2024-12-10 12:33:44 +0100

    AOC24 day 5 part 2

--- aoc24/src/bin/day_05.rs
+++ aoc24/src/bin/day_05.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/05_full.txt"));
 
 type Rule = (usize, usize);
@@ -18,24 +20,41 @@ fn parse(input: &str) -> (Vec<Rule>, Vec<Update>) {
     (rules, updates)
 }
 
+fn partition_updates(rules: &[Rule], updates: Vec<Update>) -> (Vec<Update>, Vec<Update>) {
+    updates.into_iter().partition(|update| {
+        rules.iter().all(|rule| {
+            update.iter().position(|&d| d == rule.0).is_none_or(|left| {
+                update
+                    .iter()
+                    .position(|&d| d == rule.1)
+                    .is_none_or(|right| left < right)
+            })
+        })
+    })
+}
+
 fn part_two(input: &str) -> usize {
-    0
+    let (rules, updates) = parse(input);
+    let (_, mut incorrect_updates) = partition_updates(&rules, updates);
+    incorrect_updates
+        .iter_mut()
+        .map(|update| {
+            update.sort_unstable_by(|&left, &right| {
+                rules
+                    .iter()
+                    .find(|&r| r.0 == left && r.1 == right)
+                    .map_or(Ordering::Greater, |_| Ordering::Less)
+            });
+            update[update.len() / 2]
+        })
+        .sum()
 }
 
 fn part_one(input: &str) -> usize {
     let (rules, updates) = parse(input);
-    updates
+    let (correct_updates, _) = partition_updates(&rules, updates);
+    correct_updates
         .iter()
-        .filter(|update| {
-            rules.iter().all(|rule| {
-                update.iter().position(|&d| d == rule.0).is_none_or(|left| {
-                    update
-                        .iter()
-                        .position(|&d| d == rule.1)
-                        .is_none_or(|right| left < right)
-                })
-            })
-        })
         .map(|update| update[update.len() / 2])
         .sum()
 }
@@ -56,7 +75,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 143);
         assert_eq!(part_one(INPUT), 4281);
 
-        // assert_eq!(part_two(INPUT_TEST), 123);
-        // assert_eq!(part_two(INPUT), 1912);
+        assert_eq!(part_two(INPUT_TEST), 123);
+        assert_eq!(part_two(INPUT), 5466);
     }
 }
