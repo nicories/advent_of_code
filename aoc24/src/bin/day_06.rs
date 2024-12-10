86e1da0b2b7e7813f5d83ddf4d887b357881dae2
Nico Ries <nicories@posteo.de>
2024-12-10 14:48:32 +0100

    AOC24 day 6 part 2

--- aoc24/src/bin/day_06.rs
+++ aoc24/src/bin/day_06.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
+
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/06_full.txt"));
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Right,
@@ -17,6 +19,7 @@ impl Direction {
         }
     }
 }
+#[derive(Clone, Copy)]
 enum Tile {
     Empty,
     Obstruction,
@@ -50,13 +53,8 @@ fn parse(input: &str) -> Map {
         })
         .collect()
 }
-
-fn part_two(input: &str) -> usize {
-    0
-}
-
-fn part_one(input: &str) -> usize {
-    let map = parse(input);
+// None -> loop
+fn simulate_guard(map: &Map) -> Option<Vec<Position>> {
     let start = map
         .iter()
         .enumerate()
@@ -69,6 +67,8 @@ fn part_one(input: &str) -> usize {
         .unwrap();
     let mut positions = vec![start];
     let mut dir = Direction::Up;
+    let mut loop_detector = HashSet::new();
+    loop_detector.insert((start, dir));
     loop {
         let pos = positions[positions.len() - 1];
         let next_pos = dir + pos;
@@ -81,10 +81,44 @@ fn part_one(input: &str) -> usize {
         }
         let next_tile = &map[next_pos.1 as usize][next_pos.0 as usize];
         match next_tile {
-            Tile::Empty | Tile::Start => positions.push(next_pos),
+            Tile::Empty | Tile::Start => {
+                if loop_detector.contains(&(next_pos, dir)) {
+                    return None;
+                }
+                loop_detector.insert((next_pos, dir));
+                positions.push(next_pos)
+            }
             Tile::Obstruction => dir = dir.rotate_90(),
         }
     }
+    Some(positions)
+}
+
+fn part_two(input: &str) -> usize {
+    let mut map = parse(input);
+    let coords: Vec<(usize, usize)> = map
+        .iter()
+        .enumerate()
+        .flat_map(|(y, _)| map[0].iter().enumerate().map(move |(x, _)| (x, y)))
+        .collect();
+    coords
+        .into_iter()
+        .filter(|&(x_obstruction, y_obstruction)| {
+            if matches!(map[y_obstruction][x_obstruction], Tile::Start) {
+                return false;
+            }
+            let before = map[y_obstruction][x_obstruction];
+            map[y_obstruction][x_obstruction] = Tile::Obstruction;
+            let opt = simulate_guard(&map);
+            map[y_obstruction][x_obstruction] = before.clone();
+            opt.is_none()
+        })
+        .count()
+}
+
+fn part_one(input: &str) -> usize {
+    let map = parse(input);
+    let mut positions = simulate_guard(&map).unwrap();
     positions.sort();
     positions.dedup();
     positions.len()
@@ -106,7 +140,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 41);
         assert_eq!(part_one(INPUT), 5162);
 
-        // assert_eq!(part_two(INPUT_TEST), 123);
-        // assert_eq!(part_two(INPUT), 5466);
+        assert_eq!(part_two(INPUT_TEST), 6);
+        assert_eq!(part_two(INPUT), 1909);
     }
 }
