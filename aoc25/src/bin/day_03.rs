f3f15ddaab371ad924edbf2aa4b29a07bb3f65b3
Nico Ries <nicories@posteo.de>
2025-12-05 00:29:34 +0100

    AOC25 day 3 part 2

--- aoc25/src/bin/day_03.rs
+++ aoc25/src/bin/day_03.rs
@@ -7,29 +7,43 @@ fn parse(input: &str) -> Vec<Vec<u32>> {
         .collect()
 }
 
-fn part_two(input: &str) -> usize {
-    0
+fn find_highest_batteries(pack: &Vec<u32>, num_of_batteries: u32) -> u64 {
+    let mut highest_indices: Vec<usize> = vec![];
+    for i in 0..num_of_batteries as usize {
+        let mut highest_index = if i == 0 {
+            0
+        } else {
+            highest_indices[i - 1] + 1
+        };
+        let start = highest_index + 1;
+        let stop = pack.len() - num_of_batteries as usize + i + 1;
+        for j in start..stop {
+            if pack[j] > pack[highest_index] {
+                highest_index = j;
+            }
+        }
+        highest_indices.push(highest_index);
+    }
+    highest_indices
+        .iter()
+        .enumerate()
+        .map(|(i, pack_index)| {
+            pack[*pack_index] as u64 * 10_u64.pow(num_of_batteries - 1 - i as u32)
+        })
+        .sum()
 }
 
-fn part_one(input: &str) -> u32 {
+fn part_two(input: &str) -> u64 {
     parse(input)
         .iter()
-        .map(|pack| {
-            let mut highest_index = 0;
-            for i in 1..pack.len() - 1 {
-                if pack[i] > pack[highest_index] {
-                    highest_index = i;
-                }
-            }
-            let mut second_highest_index = highest_index + 1;
-            for i in highest_index + 2..pack.len() {
-                if pack[i] > pack[second_highest_index] {
-                    second_highest_index = i;
-                }
-            }
-            dbg!(pack[highest_index], pack[second_highest_index]);
-            pack[highest_index] * 10 + pack[second_highest_index]
-        })
+        .map(|pack| find_highest_batteries(pack, 12))
+        .sum()
+}
+
+fn part_one(input: &str) -> u64 {
+    parse(input)
+        .iter()
+        .map(|pack| find_highest_batteries(pack, 2))
         .sum()
 }
 fn main() {
@@ -49,7 +63,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 357);
         assert_eq!(part_one(INPUT), 17166);
 
-        // assert_eq!(part_two(INPUT_TEST), 4174379265);
-        // assert_eq!(part_two(INPUT), 85513235135);
+        assert_eq!(part_two(INPUT_TEST), 3121910778619);
+        assert_eq!(part_two(INPUT), 169077317650774);
     }
 }
