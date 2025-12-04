741d585713b415189ad9a1f7a010aec9797cfbae
Nico Ries <nicories@posteo.de>
2025-12-04 22:35:55 +0100

    AOC25 day 2 part 1

---
+++ aoc25/src/bin/day_02.rs
@@ -0,0 +1,53 @@
+use std::ops::RangeInclusive;
+
+const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/02_full.txt"));
+
+fn id_is_invalid(id: usize) -> bool {
+    let id_str = id.to_string();
+    let (left, right) = id_str.split_at(id_str.len() / 2);
+    left == right
+}
+
+fn parse(input: &str) -> Vec<Vec<usize>> {
+    input
+        .split(',')
+        .map(|line| {
+            let (start, end) = line.split_once('-').unwrap();
+            dbg!(start, end);
+            RangeInclusive::new(start.parse().unwrap(), end.parse().unwrap()).collect()
+        })
+        .collect()
+}
+
+fn part_two(input: &str) -> usize {
+    0
+}
+
+fn part_one(input: &str) -> usize {
+    parse(input)
+        .iter()
+        .flatten()
+        .filter(|x| id_is_invalid(**x))
+        .sum()
+}
+fn main() {
+    println!("1: {}", part_one(INPUT));
+    // println!("2: {}", part_two(INPUT));
+}
+
+// test
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT_TEST: &str =
+        include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/02_test.txt"));
+
+    #[test]
+    fn test() {
+        assert_eq!(part_one(INPUT_TEST), 1227775554);
+        assert_eq!(part_one(INPUT), 64215794229);
+
+        // assert_eq!(part_two(INPUT_TEST), 6);
+        // assert_eq!(part_two(INPUT), 6254);
+    }
+}
