252514122c8a961b53108ecc94b297292825a9b1
Nico Ries <nicories@posteo.de>
2025-12-07 11:20:51 +0100

    AOC25 day 5 part 2

--- aoc25/src/bin/day_05.rs
+++ aoc25/src/bin/day_05.rs
@@ -1,14 +1,12 @@
-use std::ops::RangeInclusive;
-
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/05_full.txt"));
 
-fn parse(input: &str) -> (Vec<RangeInclusive<usize>>, Vec<usize>) {
+fn parse(input: &str) -> (Vec<(usize, usize)>, Vec<usize>) {
     let (ranges_str, ingredients_str) = input.split_once("\n\n").unwrap();
     let ranges = ranges_str
         .lines()
         .map(|line| {
             let (start_str, end_str) = line.split_once("-").unwrap();
-            RangeInclusive::new(start_str.parse().unwrap(), end_str.parse().unwrap())
+            (start_str.parse().unwrap(), end_str.parse().unwrap())
         })
         .collect();
     let ingredients = ingredients_str
@@ -19,14 +17,54 @@ fn parse(input: &str) -> (Vec<RangeInclusive<usize>>, Vec<usize>) {
 }
 
 fn part_two(input: &str) -> usize {
-    0
+    let (mut ranges, _) = parse(input);
+    ranges.sort();
+    // extend ranges
+    for (i, range) in ranges.clone().iter().enumerate() {
+        assert!(range.0 <= range.1);
+        for (j, other_range) in ranges.clone().iter().enumerate() {
+            if i == j {
+                continue;
+            }
+            assert!(other_range.0 <= other_range.1);
+            if range.0 < other_range.0 && range.1 >= other_range.0 && range.1 <= other_range.1 {
+                // end is inside filter_range
+                let change_range = ranges.get_mut(j).unwrap();
+                change_range.0 = range.0;
+            } else if range.0 >= other_range.0
+                && range.0 <= other_range.1
+                && range.1 > other_range.1
+            {
+                // start is inside filter_range
+                let change_range = ranges.get_mut(j).unwrap();
+                change_range.1 = range.1;
+            }
+        }
+    }
+    ranges.sort();
+    ranges.dedup();
+    let ranges_clone = ranges.clone();
+    // remove ranges that are inside other ranges
+    ranges.retain(|range| {
+        for other_range in &ranges_clone {
+            if range != other_range && range.0 >= other_range.0 && range.1 <= other_range.1 {
+                return false;
+            }
+        }
+        true
+    });
+    ranges.iter().map(|range| range.1 - range.0 + 1).sum()
 }
 
 fn part_one(input: &str) -> usize {
     let (ranges, ingredients) = parse(input);
     ingredients
         .iter()
-        .filter(|ingredient| ranges.iter().any(|range| range.contains(&ingredient)))
+        .filter(|ingredient| {
+            ranges
+                .iter()
+                .any(|range| **ingredient >= range.0 && **ingredient <= range.1)
+        })
         .count()
 }
 fn main() {
@@ -46,7 +84,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 3);
         assert_eq!(part_one(INPUT), 726);
 
-        // assert_eq!(part_two(INPUT_TEST), 43);
-        // assert_eq!(part_two(INPUT), 9050);
+        assert_eq!(part_two(INPUT_TEST), 14);
+        assert_eq!(part_two(INPUT), 354226555270043);
     }
 }
