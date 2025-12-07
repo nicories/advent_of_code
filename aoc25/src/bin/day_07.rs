179b40678d40faf3a766b6d82249257932acb1b9
Nico Ries <nicories@posteo.de>
2025-12-07 14:21:18 +0100

    AOC25 day 7 part 2

--- aoc25/src/bin/day_07.rs
+++ aoc25/src/bin/day_07.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 const INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/input/07_full.txt"));
 
@@ -34,8 +34,37 @@ fn parse(input: &str) -> Grid {
     Grid { tiles, start_x }
 }
 
+fn simulate(
+    grid: &Grid,
+    start_pos: (usize, usize),
+    cache: &mut HashMap<(usize, usize), usize>,
+) -> usize {
+    if let Some(timelines) = cache.get(&start_pos) {
+        return *timelines;
+    }
+    let next_pos = (start_pos.0, start_pos.1 + 1);
+    if next_pos.1 == grid.tiles.len() {
+        return 1;
+    }
+    match grid.tiles[next_pos.1][next_pos.0] {
+        Tile::Empty => simulate(grid, next_pos, cache),
+        Tile::Splitter => {
+            let left_pos = (next_pos.0 - 1, next_pos.1);
+            let right_pos = (next_pos.0 + 1, next_pos.1);
+            let left_result = simulate(grid, left_pos, cache);
+            let right_result = simulate(grid, right_pos, cache);
+            cache.insert(left_pos, left_result);
+            cache.insert(right_pos, right_result);
+            left_result + right_result
+        }
+        Tile::Start => panic!("at the disco"),
+    }
+}
+
 fn part_two(input: &str) -> usize {
-    0
+    let grid = parse(input);
+    let mut cache = HashMap::new();
+    simulate(&grid, (grid.start_x, 0), &mut cache)
 }
 
 fn part_one(input: &str) -> usize {
@@ -82,7 +111,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 21);
         assert_eq!(part_one(INPUT), 1590);
 
-        // assert_eq!(part_two(INPUT_TEST), 3263827);
+        assert_eq!(part_two(INPUT_TEST), 40);
         // assert_eq!(part_two(INPUT), 11643736116335);
     }
 }
