606169dea769a636db9a32c29d2a8fb114c15e5d
Nico Ries <nicories@posteo.de>
2025-12-07 12:37:41 +0100

    AOC25 day 6 part 2

--- aoc25/src/bin/day_06.rs
+++ aoc25/src/bin/day_06.rs
@@ -28,9 +28,52 @@ fn parse(input: &str) -> Vec<Problem> {
     }
     v
 }
+fn parse_right_to_left_in_columns(input: &str) -> Vec<Problem> {
+    let mut v = vec![];
+    let rows: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    // start upper right and go down
+    let mut current_operands = vec![];
+    for x in (0..rows[0].len()).rev() {
+        let mut operator: Option<Operator> = Option::None;
+        let mut operand_list = vec![];
+        for y in 0..rows.len() {
+            if let Some(digit) = rows[y][x].to_digit(10) {
+                operand_list.push(digit as u64);
+            }
+            operator = match rows[y][x] {
+                '+' => Some(Operator::Add),
+                '*' => Some(Operator::Multiply),
+                _ => None,
+            };
+        }
+        // skip 0 operands so multiply is not screwed up
+        if operand_list.len() != 0 {
+            current_operands.push(
+                operand_list
+                    .iter()
+                    .rev()
+                    .enumerate()
+                    .map(|(i, digit)| digit * 10_u64.pow(i as u32))
+                    .sum(),
+            );
+        }
+        if let Some(op) = operator {
+            v.push((op, current_operands.clone()));
+            current_operands.clear();
+        }
+    }
+    v
+}
 
-fn part_two(input: &str) -> usize {
-    0
+fn part_two(input: &str) -> u64 {
+    let problems = parse_right_to_left_in_columns(input);
+    problems
+        .iter()
+        .map(|(operator, operands)| match operator {
+            Operator::Add => operands.iter().sum::<u64>(),
+            Operator::Multiply => operands.iter().product(),
+        })
+        .sum()
 }
 
 fn part_one(input: &str) -> u64 {
@@ -60,7 +103,7 @@ mod tests {
         assert_eq!(part_one(INPUT_TEST), 4277556);
         assert_eq!(part_one(INPUT), 4693159084994);
 
-        // assert_eq!(part_two(INPUT_TEST), 14);
-        // assert_eq!(part_two(INPUT), 354226555270043);
+        assert_eq!(part_two(INPUT_TEST), 3263827);
+        assert_eq!(part_two(INPUT), 11643736116335);
     }
 }
